e89f325b5fb749c68136407c84b606f31962038f
Luke Robinett <lukebootcamp@gmail.com>
2021-09-07 19:56:37 -0700

    Initial commit.

---
+++ options.js
@@ -0,0 +1,2 @@
+const okButton = document.getElementById("okButton");
+okButton.addEventListener("click", () => alert("You clicked me!"));
---
+++ popup.js
@@ -0,0 +1,2 @@
+const addButton = document.getElementById("addButton");
+addButton.addEventListener("click", () => alert("Bookmark added! NOT! :p"));
