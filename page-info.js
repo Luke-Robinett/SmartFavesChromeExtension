5d4def302c57321aa300914a38a803596ea09e33
Luke Robinett <lukebootcamp@gmail.com>
2021-09-13 20:03:23 -0700

    Added page-info.html and page-info.js with dummy starter code.

---
+++ page-info.js
@@ -0,0 +1,4 @@
+var button = document.querySelector("button");
+var textarea = document.querySelector("textarea");
+
+button.addEventListener("click", () => textarea.innerHTML = "Page info goes here.");
